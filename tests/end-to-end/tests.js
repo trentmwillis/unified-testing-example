9b16ca411e8f115a6d5368843f449eb6eee575e7
Trent Willis <trentmwillis@gmail.com>
2017-07-18 19:25:52 -0700

    Add basic setup for end-to-end tests

---
+++ tests/end-to-end/tests.js
@@ -0,0 +1,67 @@
+const ChromeDevToolsProtocol = require('chrome-remote-interface');
+const ChromeLauncher = require('chrome-launcher');
+const fs = require('fs');
+
+async function browserTest(url, test) {
+  const chrome = await ChromeLauncher.launch();
+  const devtools = await ChromeDevToolsProtocol({
+    port: chrome.port
+  });
+
+  const {
+    Page,
+    Runtime
+  } = devtools;
+
+  await Page.enable();
+
+  await Page.navigate({ url });
+
+  await Page.loadEventFired();
+  // Construct test scripts to inject into page
+  const qunitConfig = `
+    QUnit = {
+      config: {
+        autostart: false
+      }
+    };
+  `;
+  const qunitPath = require.resolve('qunitjs');
+  const qunit = fs.readFileSync(qunitPath, 'utf-8');
+  const testScript = `
+    new Promise((resolve) => {
+      QUnit.on('runEnd', (data) => resolve(JSON.stringify(data)));
+      QUnit.test('testing', ${test});
+      QUnit.start();
+    });
+  `;
+
+  // Evaluate test scripts in page
+  await Runtime.evaluate({ expression: qunitConfig });
+  await Runtime.evaluate({ expression: qunit });
+
+  // TODO: Should return a Promise that resolves with the test run info
+  const testResult = await Runtime.evaluate({
+    expression: testScript,
+    awaitPromise: true
+  });
+
+  const result = JSON.parse(testResult.result.value);
+
+  devtools.close();
+  chrome.kill();
+
+  return result;
+}
+
+QUnit.test.inBrowser = function(description, url, test) {
+  QUnit.test(description, async function(assert) {
+    const result = await browserTest(url, test);
+    assert.equal(result.status, 'passed');
+  });
+};
+
+QUnit.test.inBrowser('My browser test example', 'http://localhost:8000', function(assert) {
+  assert.ok(true);
+});
+
